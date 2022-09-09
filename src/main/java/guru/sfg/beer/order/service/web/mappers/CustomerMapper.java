870caee46a88003330370e7db23d2230d1ddc013
vitor <vgc_silva@hotmail.com>
2022-09-09 10:50:13 +0100

    Adding parent BOM.

---
+++ src/main/java/guru/sfg/beer/order/service/web/mappers/CustomerMapper.java
@@ -0,0 +1,16 @@
+package guru.sfg.beer.order.service.web.mappers;
+
+
+import guru.sfg.beer.order.service.domain.Customer;
+import guru.sfg.beer.order.service.web.model.CustomerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * Created by jt on 3/7/20.
+ */
+@Mapper(uses = {DateMapper.class})
+public interface CustomerMapper {
+    CustomerDto customerToDto(Customer customer);
+
+    Customer dtoToCustomer(Customer dto);
+}
