537a96ac49e24f1370757e631a87dcd53b54ba49
vitor <vitorgilcsgmail.com>
2022-09-27 21:23:18 +0100

    Place Validate Order Request

--- src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/order/service/config/JmsConfig.java
@@ -9,9 +9,9 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
 
-    /*public static final String MY_QUEUE = "my-hello-world";
-    public static final String MY_SEND_RCV_QUEUE = "replybacktome";
-*/
+    public static final String VALIDATE_ORDER_QUEUE = "validate-order";
+    //public static final String MY_SEND_RCV_QUEUE = "replybacktome";
+
     @Bean // Serialize message content to json using TextMessage
     public MessageConverter messageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
